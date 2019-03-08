f76761c7be9bc4b5e6bef2e383af3d027c253fe8
Sumit Chhetri <sumit955188@gmail.com>
2019-03-07 18:32:26 -0800

    fix dockerignore name

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,11 @@
 import { NextFunctionComponent } from 'next'
 import axios from 'axios'
 
-const Hello: NextFunctionComponent = () => {
-  return <div>Works and updated. As seen. ok finally</div>
+const helloPage: NextFunctionComponent = () => {
+  return <div>Hello from nextJs</div>
 }
 
-Hello.getInitialProps = async () => {
+helloPage.getInitialProps = async () => {
   try {
     await axios.get('http://server:3000')
     // tslint:disable-next-line: no-console
@@ -17,4 +17,4 @@ Hello.getInitialProps = async () => {
   return {}
 }
 
-export default Hello
+export default helloPage
